4f159a99800daee8b8a464f0b5ab76b81999925c
Marianne Hoffmann <hhmaho@gmail.com>
2018-11-27 13:41:53 +0100

    add ingredients

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -3,11 +3,23 @@ import classes from "./Burger.css";
 import BurgerIngedient from "./burgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return <BurgerIngedient key={ingredientKey + i} type={ingredientKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Choose your ingredients</p>;
+  }
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngedient type="bread-top" />
-      <BurgerIngedient type="cheese" />
-      <BurgerIngedient type="meat" />
+      {transformedIngredients}
       <BurgerIngedient type="bread-bottom" />
     </div>
   );
